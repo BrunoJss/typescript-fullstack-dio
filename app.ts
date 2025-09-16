c6b92baf9da7ce91330dcac351b8613446c94f77
brunojss <bruno.jsscruz@gmail.com>
2025-09-15 23:01:46 -0300

    Tipos primitivos e declaração de variáveis

--- app.ts
+++ app.ts
@@ -1,5 +1,11 @@
-const soma = (a:number, b:number) => {
-    console.log(a + b)
-}
+// var a = 'a' //não é muito usado no mercado pelo vazamento de escopo
+let b = 'b' //implicitamente o TS atribui o tipo string à variável
+const c:string = 'c' //atribuição explicita do tipo de variável
 
-soma(1, 2)
+let n:number = 2
+let x:boolean = true
+
+let m: string | number = "Bruno" //define que a variavel aceita string e numero, detalhe no uso do |
+
+let y:any = "qualquer coisa" //define que a variavel aceita qualquer tipo de dado
+ 
